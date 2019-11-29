5127a38c9ad42803f8e148386fbd5ba7198ee98e
Dimpy <dimpy13parekh@gmail.com>
2019-11-29 16:26:03 -0500

    some design

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-//fetch the buttons
 let minusButtonPants = document.getElementById("pantsMinusButton");
 let plusButtonPants = document.getElementById("pantsAddButton");
 let minusButtonShirt = document.getElementById("shirtMinusButton");
@@ -14,71 +13,43 @@ let varSubTotal = document.getElementById("subTotal");
 let varTax = document.getElementById("taxValue");
 let varTotalValue = document.getElementById("totalValue");
 
-//create function
-
-
 function decreaseValuePants() {
-    //get quantity from innerHTML
-    //decrease by 1
     if (varPantQty.innerHTML > 0) {
         let qtyMinus = parseInt(varPantQty.innerHTML) - 1;
-        //assign that new value to the innerHTML
         varPantQty.innerHTML = qtyMinus;
 
     } else {
         varPantQty.innerHTML = 0;
     }
-    //finding extended price
     let extdPrc = parseFloat(varExtdPricePants.innerHTML);
     varExtdPricePants.innerHTML = (varPantQty.innerHTML * 40.99).toFixed(2);
 }
 
 
 function increaseValuePants() {
-    //get quantity from innerHTML\
-    //increase by 1
     let qtyAdd = parseInt(varPantQty.innerHTML) + 1;
-    //assign that new value to the innerHTML
     varPantQty.innerHTML = qtyAdd;
-
-    //finding extended price
     let extdPrc = parseFloat(varExtdPricePants.innerHTML);
     varExtdPricePants.innerHTML = (qtyAdd * 40.99).toFixed(2);
-
 }
 
 //shirt
-//create function
 function decreaseValueShirt() {
     if (varShirtQty.innerHTML > 0) {
-
-        //get quantity from innerHTML\
-        //decrease by 1
         let qtyMinus = parseInt(varShirtQty.innerHTML) - 1;
-        //assign that new value to the innerHTML
         varShirtQty.innerHTML = qtyMinus;
     } else {
         varShirtQty.innerHTML = 0;
     }
-
-    //finding extended price
     let extdPrc = parseFloat(varExtdPriceShirt.innerHTML);
     varExtdPriceShirt.innerHTML = (varShirtQty.innerHTML * 10.99).toFixed(2);
-
 }
 
-
 function increaseValueShirt() {
-    //get quantity from innerHTML\
-    //increase by 1
     let qtyAdd = parseInt(varShirtQty.innerHTML) + 1;
-    //assign that new value to the innerHTML
     varShirtQty.innerHTML = qtyAdd;
-
-    //finding extended price
     let extdPrc = parseFloat(varExtdPriceShirt.innerHTML);
     varExtdPriceShirt.innerHTML = (qtyAdd * 10.99).toFixed(2);
-
 }
 
 function calculate() {
@@ -93,13 +64,11 @@ function promo() {
     if (pCode == "NOTAX") {
         varTax.innerHTML = 0;
         varTotalValue.innerHTML = (parseFloat(varSubTotal.innerHTML) + parseFloat(varTax.innerHTML)).toFixed(2);
-
     } else if (pCode == "FIFTYFIFTY") {
         varSubTotal.innerHTML = (parseFloat(varSubTotal.innerHTML) * 50 / 100).toFixed(2);
         varTotalValue.innerHTML = (parseFloat(varSubTotal.innerHTML) + parseFloat(varTax.innerHTML)).toFixed(2);
     }
 }
-//add event listener
 minusButtonPants.addEventListener("click", decreaseValuePants);
 plusButtonPants.addEventListener("click", increaseValuePants);
 minusButtonShirt.addEventListener("click", decreaseValueShirt);
